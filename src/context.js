469776049251f1ccf8286fbd0832ade520ae8450
Eskender <esken999@gmail.com>
2020-10-24 14:41:58 +0300

    adding StyledHero and SingleRoom components

--- src/context.js
+++ src/context.js
@@ -15,7 +15,6 @@ class RoomProvider extends Component {
   componentDidMount() {
     // this.getData
     let rooms = this.formatData(items);
-    console.log(rooms);
     let featuredRooms = rooms.filter((room) => room.featured === true);
     this.setState({
       rooms,
@@ -34,10 +33,15 @@ class RoomProvider extends Component {
     });
     return tempItems;
   }
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
 
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import StyledHero from "../components/StyledHero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export default class SingleRoom extends Component {
-    render() {
-        return (
-            <div>
-                Hello From SingleRoom
-            </div>
-        )
+  constructor(props, context) {
+    super(props, context);
+    //   console.log(props, context);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg,
+    };
+  }
+  //   componentDidMount() {}
+  static contextType = RoomContext;
+
+  render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>No room could be found...</h3>
+          <Link to="/rooms" className="btn-primary">
+            Back to Rooms
+          </Link>
+        </div>
+      );
     }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images,
+    } = room;
+    const [mainImg, ...defaultImg] = images;
+    return (
+      <>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              Back to Rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+        </section>
+      </>
+    );
+  }
 }

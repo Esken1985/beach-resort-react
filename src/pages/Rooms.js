474c42b89e3410da2ce956813821d45c80d6f5df
Eskender <esken999@gmail.com>
2020-10-26 05:56:37 +0300

    adding RoomsContainer, RoomsFilter and RoomsList components

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Banner from '../components/Banner'
-import Hero from '../components/Hero'
+import React from "react";
+import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
+import Hero from "../components/Hero";
+import RoomsContainer from "../components/RoomsContainer";
 
 const Rooms = () => {
-    return (
-        <Hero hero="roomsHero" >
-            <Banner title="Our Rooms" >
-                <Link to="/" className="btn-primary">
-                    Return Home
-                </Link>
-            </Banner>
-        </Hero>
-    )
-}
+  return (
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="Our Rooms">
+          <Link to="/" className="btn-primary">
+            Return Home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer />
+    </>
+  );
+};
 
-export default Rooms
+export default Rooms;
